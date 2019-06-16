267a1139777a7ac17dcacfa7c23f7406dc91035f
yuhaibao324 <yuhaibao324@126.com>
2019-06-16 22:30:33 +0800

    Spring配置

--- src/main/java/com/heima/springboot/javaconfig/SpringConfig.java
+++ src/main/java/com/heima/springboot/javaconfig/SpringConfig.java
@@ -1,8 +1,13 @@
 package com.heima.springboot.javaconfig;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 /**
  * @创建人 steffens
@@ -11,12 +16,48 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration //通过该注解来表明该类是一个Spring的配置，相当于一个xml文件
 @ComponentScan(basePackages = "com.heima.springboot.javaconfig") //配置扫描包
+@PropertySource(value= {"classpath:jdbc.properties"}, ignoreResourceNotFound = true)
 public class SpringConfig {
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${jdbc.driverClassName}")
+    private String jdbcDriverClassName;
+
+    @Value("${jdbc.username}")
+    private String jdbcUsername;
+
+    @Value("${jdbc.password}")
+    private String jdbcPassword;
 
     @Bean // 通过该注解来表明是一个Bean对象，相当于xml中的<bean>
     public UserDAO getUserDAO(){
         return new UserDAO(); // 直接new对象做演示
     }
 
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
+        // 数据库驱动
+        boneCPDataSource.setDriverClass(jdbcDriverClassName);
+        // 相应驱动的jdbcUrl
+        boneCPDataSource.setJdbcUrl(jdbcUrl);
+        // 数据库的用户名
+        boneCPDataSource.setUsername(jdbcUsername);
+        // 数据库的密码
+        boneCPDataSource.setPassword(jdbcUsername);
+        // 检查数据库连接池中空闲连接的间隔时间，单位是分，默认值：240，如果要取消则设置为0
+        boneCPDataSource.setIdleConnectionTestPeriodInMinutes(60);
+        // 连接池中未使用的链接最大存活时间，单位是分，默认值：60，如果要永远存活设置为0
+        boneCPDataSource.setIdleMaxAgeInMinutes(30);
+        // 每个分区最大的连接数
+        boneCPDataSource.setMaxConnectionsPerPartition(100);
+        // 每个分区最小的连接数
+        boneCPDataSource.setMinConnectionsPerPartition(5);
+        return boneCPDataSource;
+    }
+
 }
+
 
